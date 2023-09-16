c57f670cd6200f8f7307f7d06ee32fcb2f369cf0
kasDev18 <87753378+kasDev18@users.noreply.github.com>
2023-09-16 21:04:25 +0800

    modify index UI

--- routes/landing.js
+++ routes/landing.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const landing = require('../controllers/landing');
+const { isLoggedIn } = require('../middleware');
 // const Users = require('../models/User');
 
 // const wrapAsync = require('../utils/catchAsync');
 
 router.route('/index')
-    .get(landing.index);
+    .get(isLoggedIn,
+        landing.index);
 
 module.exports = router;
