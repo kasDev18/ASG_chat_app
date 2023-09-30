84f3d297677db6f78e81693c8af682a147d2bdc7
kasDev18 <87753378+kasDev18@users.noreply.github.com>
2023-09-30 23:14:59 +0800

    added boilerplate

--- controllers/landing.js
+++ controllers/landing.js
@@ -4,7 +4,9 @@ module.exports.index = async(req, res) => {
     if (req.isAuthenticated()) {
         const userID = req.user._id;
         const userEmail = req.user.username;
+        const userName = req.user.name;
+        // console.log(userName);
         const users = await Users.find({ _id: { $ne: userID } });
-        res.render('landing/index' , { users, userID, userEmail });
+        res.render('landing/index' , { users, userID, userEmail, userName });
     }
 }
